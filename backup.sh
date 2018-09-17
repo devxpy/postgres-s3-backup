543bf16c2d77190c66fd36f6c0ca23c5a1815ef5
dev <devxpy@gmail.com>
2018-09-17 14:57:55 +0530

    ...

--- backup.sh
+++ backup.sh
@@ -2,6 +2,4 @@
 
 filename=./$(date '+%a %W %b %Y %X')
 
-pg_dump dbname > "$filename"
-
-aws s3 cp "$filename" s3://saral-data-bucket/backups/
+pg_dump dbname > "$filename" && aws s3 cp "$filename" s3://saral-data-bucket/backups/
