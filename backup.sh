a3bac4f22b21ea97f0c6585da7337443ff0477fd
dev <devxpy@gmail.com>
2018-09-17 16:49:26 +0530

    fix timezone

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-filename=./$(date '+%a %d %b %Y %r')
+filename=./$(env TZ=Asia/Kolkata date '+%a %d %b %Y %r')
 
 pg_dump > "$filename" && aws s3 cp "$filename" "s3://$AWS_STORAGE_BUCKET_NAME/backups/"
