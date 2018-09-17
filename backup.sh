c2d647b04f7d0c60069f17320ff2991b6a27f69c
dev <devxpy@gmail.com>
2018-09-17 15:04:42 +0530

    supply proper credentials to postgres

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 
 filename=./$(date '+%a %W %b %Y %X')
 
-pg_dump dbname > "$filename" && aws s3 cp "$filename" s3://saral-data-bucket/backups/
+pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME > "$filename" \
+&& aws s3 cp "$filename" "s3://$AWS_STORAGE_BUCKET_NAME/backups/"
